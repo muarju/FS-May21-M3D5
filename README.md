68c214300e94799446939e5f8353fb425512b3ec
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-06-25 17:22:53 +0200

    fixed

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# FS-May21-M3D5
+Watch <a href="https://muarju.github.io/FS-May21-Build-week-1/" target="_blank">live demo</a>
+
+# FS-May21-Build-week-1
+ Build-Week-Project
+The goal is to create a copy of the Spotify Interface.
+The required pages are:
+
+- Login page
+- Home page
+- Artist page
+- Album page
+
+ 
+Hints:
+
+> Organize your styles in order to have reusable classes amongst the whole project / different pages
+
+> Divide the tasks in a super clear way 
+
+> Finish one single page before moving to the next one
+
+> Keep an "always-on but muted" channel so that if you need help, you can just open the mic and speak out loud
+
+> Commit and merge often, try to work on separate branches.
--- js/showRunner.js
+++ js/showRunner.js
@@ -30,7 +30,7 @@ displayCards(
 //* END -- Good Morning Section
 //! ------------------------------------< Card for the next sections >-------------------------------------
 const card = spotify =>`
-
+<a href="album.html?id=${ spotify.id }">
 <div class="col">
     <div class="card mb-3">
     <img src=${ spotify.img } alt="...">
@@ -40,6 +40,7 @@ const card = spotify =>`
     </div>
     </div>
 </div>
+</a>
 `
 
 //! ------------------------------------< Recently played Section >----------------------------------------
@@ -95,10 +96,10 @@ const albums = (albumList) => {
 
 /* code for artist page */
 const artistData = (albumList) => {
-
+    const albumTablec=''
     const albumTable = document.querySelector("#artist-table");
     albumList.forEach( (albumInfo, i) => {
-        albumTable.innerHTML +=`
+        albumTablec +=`
         <tr>
             <td>${ i+1 }</td>
             <td>
@@ -110,7 +111,84 @@ const artistData = (albumList) => {
         </tr>
         `
     });
+    albumTable.innerHTML=albumTablec
   };
+
+
+  
+function artistTable(mydata) {
+    const artistpageTable = document.querySelector("#artist-table1")
+    const  artistTopTracks  = mydata
+    console.log(artistTopTracks)
+    let tr = ""
+    artistTopTracks.forEach((items,i) => {
+        tr += `<tr>
+                <td>${ i+1 }</td>
+                    <td>
+                        <img src="${ items.images[2].url }" style="height: 40px;float: left;padding-right: 10px;">
+                        <div class="song-title">${ items.name }</div>
+                    </td>
+                    <td>${items.rank }</td>
+                    <td>${ songLength( items.duration ) }</td>
+                </tr>
+            `
+    })
+    artistpageTable.innerHTML = tr
+  }
+
+
+
+  
+function new_releases(mydata) {
+    const new_releasesSection = document.querySelector(".new-releases-cards")
+    const  spotify  = mydata
+    const  spotifyitem  = spotify.albums.items
+    let cards = ""
+    spotifyitem.forEach((items) => {
+        cards += `
+            <div class="col">
+            <a href="album.html?id=${ items.id }">
+                <div class="card mb-3">
+                <img src=${ items.images[0].url } alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-truncate">${ items.name } </h5>
+                    <p ><small>Last updated </small></p>
+                </div>
+                </div>
+                </a>
+            </div>
+            `
+    })
+    new_releasesSection.innerHTML = cards
+  }
+
+  
+  function albumdetails(mydata) {
+    const album_name = document.querySelector("#album_name")
+    const artistname = document.querySelector("#artistname")
+    const albumimage = document.querySelector("#album-image")
+
+    const  spotifyAlbumDetails  = mydata
+    album_name.innerHTML=spotifyAlbumDetails.name
+    artistname.innerHTML=spotifyAlbumDetails.artists[0].name
+    artistname.href=`artist.html?artistid=${spotifyAlbumDetails.artists[0].id}`
+    albumimage.innerHTML=
+    `<img src="${spotifyAlbumDetails.images[0].url}" class="img-fluid">`
+
+  }
+
+  function artistdetails(mydata) {
+    const artistName = document.querySelector("#artistName")
+    const followers= document.querySelector(".followers")
+    const maincontentartist = document.querySelector("#main-content-artist")
+    const  artistDetails  = mydata
+    artistName.innerHTML=artistDetails.name
+    followers.innerHTML=artistDetails.followers.total
+    maincontentartist.style=`background-image: url(${artistDetails.images[0].url})`
+
+
+  }
+
 
   window.onload = function () {
     fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=Bohemian Rhapsody (The Original Soundtrack)")
@@ -125,4 +203,47 @@ const artistData = (albumList) => {
       .then((data) => data.data)
       .then(artistData)
       .catch(console.log);
+
+        fetch("https://api.spotify.com/v1/browse/new-releases", {
+        headers: {
+            Authorization: "Bearer BQCVvewcQ30esNYbURaGQIvfXGbAR84UV1KseA2BB1OMfYfWDmWXaaqRfFGoMvf3mUKMHXpyROm6nggBvhwyGuniDrx_4OwqO-MYaZCnBypL5wJDebDH_kpCNCyxYVULyObO9DooptZXe7hFIskXpnW8ztcpZuEDi0Y",
+        },
+        })
+        .then((data) => data.json())
+        .then((mydata) => new_releases(mydata))
+        .catch((error) => console.error(error))
+
+        //album page id
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const albumid = urlParams.get('id')
+        const artistid = urlParams.get('artistid')
+
+        fetch("https://api.spotify.com/v1/albums/" +albumid , {
+        headers: {
+            Authorization: "Bearer BQCVvewcQ30esNYbURaGQIvfXGbAR84UV1KseA2BB1OMfYfWDmWXaaqRfFGoMvf3mUKMHXpyROm6nggBvhwyGuniDrx_4OwqO-MYaZCnBypL5wJDebDH_kpCNCyxYVULyObO9DooptZXe7hFIskXpnW8ztcpZuEDi0Y",
+        },
+        })
+        .then((data) => data.json())
+        .then((mydata) => albumdetails(mydata))
+        .catch((error) => console.error(error))
+
+        fetch("https://api.spotify.com/v1/artists/" +artistid , {
+        headers: {
+            Authorization: "Bearer BQCVvewcQ30esNYbURaGQIvfXGbAR84UV1KseA2BB1OMfYfWDmWXaaqRfFGoMvf3mUKMHXpyROm6nggBvhwyGuniDrx_4OwqO-MYaZCnBypL5wJDebDH_kpCNCyxYVULyObO9DooptZXe7hFIskXpnW8ztcpZuEDi0Y",
+        },
+        })
+        .then((data) => data.json())
+        .then((mydata) => artistdetails(mydata))
+        .catch((error) => console.error(error))
+        fetch("https://api.spotify.com/v1/artists/"+artistid+"/top-tracks?market=ES"  , {
+            headers: {
+                Authorization: "Bearer BQCVvewcQ30esNYbURaGQIvfXGbAR84UV1KseA2BB1OMfYfWDmWXaaqRfFGoMvf3mUKMHXpyROm6nggBvhwyGuniDrx_4OwqO-MYaZCnBypL5wJDebDH_kpCNCyxYVULyObO9DooptZXe7hFIskXpnW8ztcpZuEDi0Y",
+            },
+            })
+            .then((data) => data.json())
+            .then((mydata) => artistTable(mydata))
+            .catch((error) => console.error(error))
+
+
   };
